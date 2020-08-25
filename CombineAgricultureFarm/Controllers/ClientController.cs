1a00a1c2dd580b59bf03e63d2e11332d75b22808
Hridoy Roy <15103182@iubat.edu>
2020-08-26 02:42:43 +0600

    First normla login part complete

--- CombineAgricultureFarm/Controllers/ClientController.cs
+++ CombineAgricultureFarm/Controllers/ClientController.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using CombineAgricultureFarm.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using System.Web.UI.WebControls;
+
 namespace CombineAgricultureFarm.Controllers
 {
+   
     public class ClientController : Controller
     {
+
+        Models.CombineAgricultureFarmEntities db = new CombineAgricultureFarmEntities();
         // GET: Client
         public ActionResult Index()
         {
@@ -17,6 +23,58 @@ namespace CombineAgricultureFarm.Controllers
         public ActionResult SignUp()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp([Bind(Include = "ClientId,ClientName,ClientEmail,ClientMobile,ClientAddress,FurmName,UserName,Password,ClientType")] ClientInformation clientInformation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ClientInformations.Add(clientInformation);
+                db.SaveChanges();
+                return RedirectToAction("SignIn");
+            }
+
+            return View(clientInformation);
+           
+        }
+        
+        [HttpGet]
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignIn(String username, String password)
+        {
+            int er = 0;
+            if (username == "")
+            {
+                er++;
+                ViewBag.username = "Username required";
+            }
+            if (password == "")
+            {
+                er++;
+                ViewBag.password = "Password required";
+            }
+            if (er > 0)
+            {
+                return View();
+            }
+            var logininfo = db.ClientInformations.Where(x => x.UserName == username && x.Password == password).FirstOrDefault();
+            if (logininfo != null)
+            {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                ViewBag.message = "Login fail";
+                return View(logininfo);
+            }
+
+            
         }
     }
 }
--- CombineAgricultureFarm/Models/ClientInformation.cs
+++ CombineAgricultureFarm/Models/ClientInformation.cs
@@ -28,6 +28,7 @@ namespace CombineAgricultureFarm.Models
         public string FurmName { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string ClientType { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<FarmInformation> FarmInformations { get; set; }

022da43793e542ebf4ca6c564210ee0752de26c2
Hridoy Roy <15103182@iubat.edu>
2020-08-28 19:40:21 +0600

    Done FarmType, Farm session, Crop Information

---
+++ CombineAgricultureFarm/Controllers/CropInformationController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CombineAgricultureFarm.Models;
+
+namespace CombineAgricultureFarm.Controllers
+{
+    public class CropInformationController : Controller
+    {
+        Models.CombineAgricultureFarmEntities db = new CombineAgricultureFarmEntities();
+        // GET: CropInformation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CropInfo()
+        {
+            return View(db.CropInformations.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult CropInfoCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CropInfoCreate([Bind(Include = "CropInfod,CropName,CropInformation1,CropMedicine,CropPicture")] CropInformation cropInformation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CropInformations.Add(cropInformation);
+                db.SaveChanges();
+                return RedirectToAction("CropInfo");
+            }
+            return View(cropInformation);
+        }
+
+        [HttpGet]
+        public ActionResult CropInfoUpdate(int?id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            CropInformation cropInformation = db.CropInformations.Find(id);
+            if (cropInformation==null)
+            {
+                HttpNotFound();
+            }
+            return View(cropInformation);
+        }
+
+        [HttpPost]
+        public ActionResult CropInfoUpdate([Bind(Include = "CropInfod,CropName,CropInformation1,CropMedicine,CropPicture")] CropInformation cropInformation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cropInformation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("CropInfo");
+            }
+            return View(cropInformation);
+        }
+
+        [HttpGet]
+        public ActionResult CropInfoDelete(int?id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CropInformation cropInformation = db.CropInformations.Find(id);
+            if (cropInformation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cropInformation);
+        }
+
+        [HttpPost]
+        public ActionResult CropInfoDelete(int id)
+        {
+            CropInformation cropInformation = db.CropInformations.Find(id);
+            db.CropInformations.Remove(cropInformation);
+            db.SaveChanges();
+            return RedirectToAction("CropInfo");
+        }
+        
+    }
+}
--- CombineAgricultureFarm/Controllers/FarmInfoController.cs
+++ CombineAgricultureFarm/Controllers/FarmInfoController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -55,6 +56,128 @@ namespace CombineAgricultureFarm.Controllers
             }
             return View(farmingType);
         }
+
+        [HttpPost]
+        public ActionResult FarmTypeUpdated([Bind(Include = "FarmingTypeId,FarmingTyeName")] FarmingType farmingType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(farmingType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("FarmType");
+            }
+            return View(farmingType);
+        }
+
+        [HttpGet]
+        public ActionResult FarmTypeDelete(int?id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FarmingType farmingType = db.FarmingTypes.Find(id);
+            if (farmingType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(farmingType);
+        }
+
+
+        [HttpPost]
+        public ActionResult FarmTypeDelete(int id)
+        {
+           // ClientInformation clientInformation = db.ClientInformations.Find(id);
+            FarmingType farmingType = db.FarmingTypes.Find(id);
+            db.FarmingTypes.Remove(farmingType);
+            db.SaveChanges();
+            return RedirectToAction("FarmType");
+        }
+
+        // End farmtype 
+
+        // Start farm session
+
+        public ActionResult FarmSessiontype()
+        {
+            return View(db.FarmingSessionInfoes.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult FarmingSessionCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FarmingSessionCreate([Bind(Include = "SessionInfoId,SessionInfoName")] FarmingSessionInfo farmingSessionInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FarmingSessionInfoes.Add(farmingSessionInfo);
+                db.SaveChanges();
+                return RedirectToAction("FarmSessiontype");
+            }
+
+            return View(farmingSessionInfo);
+        }
+
+        [HttpGet]
+        public ActionResult FarmingSessionUpdate(int?id) 
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+
+            FarmingSessionInfo farmingSessionInfo = db.FarmingSessionInfoes.Find(id);
+            if (farmingSessionInfo==null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(farmingSessionInfo);
+        }
+
+        [HttpPost]
+        public ActionResult FarmingSessionUpdate([Bind(Include = "SessionInfoId,SessionInfoName")] FarmingSessionInfo farmingSessionInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(farmingSessionInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("FarmSessiontype");
+            }
+            return View(farmingSessionInfo);
+        }
+
+        [HttpGet]
+        public ActionResult FarmingSessionDelete(int?id)
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FarmingSessionInfo farmingSessionInfo = db.FarmingSessionInfoes.Find(id);
+            if (farmingSessionInfo == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(farmingSessionInfo);
+        }
+
+        [HttpPost]
+        public ActionResult FarmingSessionDelete(int id)
+        {
+            FarmingSessionInfo farmingSessionInfo = db.FarmingSessionInfoes.Find(id);
+            db.FarmingSessionInfoes.Remove(farmingSessionInfo);
+            db.SaveChanges();
+            return RedirectToAction("FarmSessiontype");
+        }
+
 
     }
 }
